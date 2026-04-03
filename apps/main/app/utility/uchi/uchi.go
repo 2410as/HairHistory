10dc04c946aa84edc82cd150c3fdd02f0cead09c
2410as <kmthtk937@gmail.com>
2026-04-03 21:44:17 +0900

    refactor(api): uchiラッパーを削除しchi.NewRouterに統一

--- apps/main/app/utility/uchi/uchi.go
+++
@@ -1,9 +0,0 @@
-package uchi
-
-import "github.com/go-chi/chi/v5"
-
-// NewRouter is a thin wrapper around chi.NewRouter.
-// This exists to match the "uchi.NewRouter()" initialization style you requested.
-func NewRouter() chi.Router {
-	return chi.NewRouter()
-}
